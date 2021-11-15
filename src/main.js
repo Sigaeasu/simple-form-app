a59f3500ea225d6826aa704a4d29f6b817d87149
sigaeasu <sigikemahduta@gmail.com>
2021-11-15 21:53:07 +0700

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-//import Vue Router
 import VueRouter from 'vue-router'
 
-//menggunkan Vue Router di Vue JS
 Vue.use(VueRouter);
+
+//import bootstrap css & js
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
 
 //import components
 import PostsIndex from './components/posts/Index'
